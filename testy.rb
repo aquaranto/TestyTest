dcbcb06242ddd35a5eed354e9963491dc0eca357
aquaranto <amanda@quaran.to>
2013-02-18 19:32:10 -0500

    Initial Commit

---
+++ testy.rb
@@ -0,0 +1,7 @@
+bananas = "This is only a test"
+
+puts "Regular: " + bananas + "."
+puts "Upcase: " + bananas.upcase + "."
+puts "Downcase: #{bananas.downcase}."
+puts "Reverse: #{bananas.reverse}."
+puts "The phrase is #{bananas.length} charaters long."
